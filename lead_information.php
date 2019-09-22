d6a71a0f3ceafb491d567a523a71480eb422f2ef
sashrikap <43736206+sashrikap@users.noreply.github.com>
2019-09-22 02:27:59 -0700

    Add files via upload
    Website with all components and information

---
+++ lead_information.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <title>Lead Concentration Analysis</title>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Pontano+Sans&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="css/style.css" rel="stylesheet">
+    <style>
+	.parallax {
+	  /* The image used */
+	  background-image: linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url('images/blue-gradient.jpg');
+
+	  /* Set a specific height */
+	  min-width: 100%;
+	  min-height: 100vh;
+	  /* Create the parallax scrolling effect */
+	  background-attachment: fixed;
+	  background-position: center;
+	  background-repeat: no-repeat;
+	  background-size: cover;
+	}
+	</style>
+  </head>
+
+  <body> 
+  	<div class="parallax">
+  		<h1 class="main-title" style="padding-top: 10%;">What LEAD to this?</h1>
+		<div id="inputform"> 
+			<h3 style="margin: 0px 100px;">Ever since the outcry over Flint, Michigan's heightened lead concentration in public water systems, which were used by facilities including schools, there has been a greater demand for quality water. To analyze how lead poisoning could potentially impact schoolchildren across the United States, we visualize lead concentration in schools in the populous state of New York.</h3>
+		</div>
+		<a href="#icon-section"><i class="fa fa-angle-down" aria-hidden="true" style="color:white; font-size: 80px;position:relative;left:50%;transform:translate(-50%,0%); margin: 50px 0;"></i></a>
+
+		<div id="icon-section" style="padding: 30px 0;">
+			<div style="border: 1px solid white; border-radius: 4px; width: 80%; position:relative;left:50%;transform:translate(-50%,0%); margin-bottom: 30px; background-color:white;">
+				<i class="fa fa-heartbeat" aria-hidden="true" style="color:#1a3e6b;font-size:80px;position:relative;left:50%;transform:translate(-50%,0%); margin: 30px 0px;"></i>
+				<br>
+				<h1 style="text-align:center;color:#1a3e6b;">Health</h1>
+				<p style="text-align:center; font-size: 30px; margin-bottom: 30px; padding: 25px;color:#1a3e6b">In 2017, <a href="https://vox.com/science-and-health/2017/4/27/15424050/us-underreports-lead-poisoning-cases-map-community" style="text-decoration: none; color:#1a3e6b">Vox reported that 1.2 million of United States children suffered from lead poisoning</a>, and with the disproportionate amount of treatment delivered - especially considering that it is not mandatory to test children for lead - the risk of children being poisoned by lead persists.</p> 
+			</div>
+
+			<div style="border: 1px solid white; border-radius: 4px; width: 80%; position:relative;left:50%;transform:translate(-50%,0%); margin-bottom: 30px; background-color:white;">
+				<i class="fa fa-money" aria-hidden="true" style="color:#1a3e6b;font-size:80px;position:relative;left:50%;transform:translate(-50%,0%); margin: 30px 0px;"></i>
+				<br>
+				<h1 style="text-align:center;color:#1a3e6b;">Cost</h1>
+				<p style="text-align:center; font-size: 30px; margin-bottom: 30px; color:#1a3e6b;padding: 25px;"><a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2717145/" style="text-decoration: none; color:#1a3e6b">The National Center for Biotechnology Information estimates that, for every dollar invested in controlling the proliferation of lead paint, 17 to 221 dollars can be returned. Placing that quantity in the context of the full extent of the issue, about 181 to 269 billion dollars may be returned if action is taken.</a></p> 
+			</div>
+
+			<div style="border: 1px solid white; border-radius: 4px; width: 80%; position:relative;left:50%;transform:translate(-50%,0%); margin-bottom: 30px; background-color:white;">
+				<i class="fa fa-cog" aria-hidden="true" style="color:#1a3e6b;font-size:80px;position:relative;left:50%;transform:translate(-50%,0%); margin: 30px 0px;"></i>
+				<br>
+				<h1 style="text-align:center;color:#1a3e6b;">Industry</h1>
+				<p style="text-align:center; font-size: 30px; margin-bottom: 30px; padding: 25px;color:#1a3e6b"><a href="https://www.who.int/news-room/fact-sheets/detail/lead-poisoning-and-health" style="text-decoration: none; color:#1a3e6b">The World Health Organization enumerates sources of exposure to lead</a>, emphasizing that any level of lead contamination could be considered unsafe. From its use in manufacturing and mining - and extending to its presence in certain fuels and paints - lead exposure is a serious concern, especially when it comes to young children.</p> 
+			</div>
+
+			<div style="border: 1px solid white; border-radius: 4px; width: 80%; position:relative;left:50%;transform:translate(-50%,0%); margin-bottom: 30px; background-color:white;">
+				<i class="fa fa-bullhorn" aria-hidden="true" style="color:#1a3e6b;font-size:80px;position:relative;left:50%;transform:translate(-50%,0%); margin: 30px 0px;"></i>
+				<br>
+				<h1 style="text-align:center;color:#1a3e6b;">Social Repercussions</h1>
+				<p style="text-align:center; font-size: 30px; margin-bottom: 30px; padding: 25px;color:#1a3e6b"><a href="https://www.healthaffairs.org/do/10.1377/hblog20170808.061398/full/" style="text-decoration: none; color:#1a3e6b">HealthAffairs discussed the correlation between lead poisoning and low incoming communities, concluding that the strong correlation illustrated the effects of government inaction.</a> As the crisis posed by lead poisoning festers, children growing up in areas subject to penury are put directly at risk as a result of their surroundings.</p> 
+			</div>
+
+			<br><br><br>
+		</div>
+  	</div>
+
+  	<h1 style="color:'#326cb3'; text-align:center; padding: 30px;"><a href="index.php" style="text-decoration:none;">Search with another ZIP Code</a></h1>
+
+<!--   	<div id="description">
+  		<h1>Lead Concentration Analysis</h1>
+  		<h3>Ever since the outcry over Flint, Michigan's heightened lead concentration in public water systems, which were used by facilities including schools, there has been a greater demand for quality water. To analyze how lead poisoning could potentially impact schoolchildren across the United States, we visualize lead concentration in schools in the populous state of New York.</h3>
+  	</div> -->
+ 
+
+  </body>
+
+</html>
