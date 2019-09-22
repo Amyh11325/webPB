225c37f7724a90e3475e64115e9c883d91711fd3
Amy He <39035908+Amyh11325@users.noreply.github.com>
2019-09-22 00:03:04 -0700

    Update LeadMapZipCode.php

--- LeadMapZipCode.php
+++ LeadMapZipCode.php
@@ -1,5 +1,5 @@
 <html><head>
-    <title>Leaflet.heat demo</title>
+    <title>Lead Heatmap</title>
     <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.css">
     <script src="http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.js"></script>
     <style>
